de1d5345833b50bd94ea6f784a69be978d2799e1
Spencer Davis <spencerfdavis@gmail.com>
2017-07-13 14:04:07 -0600

    Handle failed airbrake notifications

--- index.js
+++ index.js
@@ -1,14 +1,27 @@
-const { curryN } = require('crocks')
+const { curryN }         = require('crocks')
+const { flip, is, when } = require('ramda')
+
+const addReqData = (err, req) =>
+  when(is(Error), flip(Object.assign)({
+    action     : req.pathname,
+    component  : 'paperplane',
+    httpMethod : req.method,
+    params     : req.params,
+    ua         : req.headers['user-agent'],
+    url        : req.protocol + '://' + req.headers.host + req.url
+  }))(err)
 
 const handle = curryN(3, (airbrake, req, err) => {
-  err.action     = req.pathname
-  err.component  = 'paperplane'
-  err.httpMethod = req.method
-  err.params     = req.params
-  err.ua         = req.headers['user-agent']
-  err.url        = req.protocol + '://' + req.headers.host + req.url
-  airbrake.notify(err)
-  return Promise.reject(err)
+  const error = addReqData(err, req)
+
+  airbrake.notify(error, when(is(Error), e => {
+    const notifyErr = addReqData(e, req)
+    notifyErr.data = { err }
+
+    airbrake.notify(notifyErr, when(is(Error), console.error))
+  }))
+
+  return Promise.reject(error)
 })
 
 module.exports = curryN(2, (airbrake, app) => req =>
