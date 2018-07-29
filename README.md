d0662d50e37799037abdf4a5be2f9e9a911ff11d
Scott McCormack <scott.mccormack@articulate.com>
2018-07-28 23:19:55 -0400

    Full coverage

--- README.md
+++ README.md
@@ -5,7 +5,7 @@
   An <a href="https://github.com/airbrake/node-airbrake"><code>airbrake</code></a> wrapper for <a href="https://github.com/articulate/paperplane"><code>paperplane</code></a>.
 </p>
 <p align="center">
-  <a href="https://www.npmjs.com/package/paperplane-airbrake"><img src="https://img.shields.io/npm/v/paperplane-airbrake.svg" alt="npm version" style="max-width:100%;"></a> <a href="https://www.npmjs.com/package/paperplane-airbrake"><img src="https://img.shields.io/npm/dm/paperplane-airbrake.svg" alt="npm downloads" style="max-width:100%;"></a> <a href="https://travis-ci.org/articulate/paperplane-airbrake"><img src="https://travis-ci.org/articulate/paperplane-airbrake.svg?branch=master" alt="Build Status" style="max-width:100%;"></a>
+  <a href="https://www.npmjs.com/package/paperplane-airbrake"><img src="https://img.shields.io/npm/v/paperplane-airbrake.svg" alt="npm version" style="max-width:100%;"></a> <a href="https://www.npmjs.com/package/paperplane-airbrake"><img src="https://img.shields.io/npm/dm/paperplane-airbrake.svg" alt="npm downloads" style="max-width:100%;"></a> <a href="https://travis-ci.org/articulate/paperplane-airbrake"><img src="https://travis-ci.org/articulate/paperplane-airbrake.svg?branch=master" alt="Build Status" style="max-width:100%;"></a> <a href='https://coveralls.io/github/articulate/paperplane-airbrake?branch=v2'><img src='https://coveralls.io/repos/github/articulate/paperplane-airbrake/badge.svg?branch=v2' alt='Coverage Status' /></a>
 </p>
 
 ## Usage
--- test/index.js
+++ test/index.js
@@ -3,6 +3,38 @@ const property     = require('prop-factory')
 const { redirect } = require('paperplane')
 const spy          = require('@articulate/spy')
 
+const { partial, path } = require('ramda')
+
+const itAddsRequestDetailsTo = err => {
+  it('rejects with the error', () =>
+    expect(err()).to.exist
+  )
+
+  it('adds the action', () =>
+    expect(err().action).to.equal('/users/123')
+  )
+
+  it('adds paperplane as the component', () =>
+    expect(err().component).to.equal('paperplane')
+  )
+
+  it('adds the httpMethod', () =>
+    expect(err().httpMethod).to.equal('GET')
+  )
+
+  it('adds the params', () =>
+    expect(err().params).to.eql({ id: 123 })
+  )
+
+  it('adds the ua (user agent)', () =>
+    expect(err().ua).to.equal('Chrome')
+  )
+
+  it('adds the url', () =>
+    expect(err().url).to.equal('https://myapp.com/users/123?token=abc')
+  )
+}
+
 describe('paperplane-airbrake', () => {
   const airbrake = { notify: spy() }
 
@@ -25,45 +57,19 @@ describe('paperplane-airbrake', () => {
   )
 
   describe('when app errors', () => {
-    const bad = Object.assign(new Error('bad'), { req })
     const err = property()
 
-    beforeEach(() =>
-      cry(bad).catch(err)
-    )
+    beforeEach(() => {
+      const bad = Object.assign(new Error('bad'), { req })
+      return cry(bad).catch(err)
+    })
 
     it('notifies airbrake', () => {
       expect(airbrake.notify.calls.length).to.equal(1)
       expect(airbrake.notify.calls[0][0]).to.equal(err())
     })
 
-    it('rejects with the error', () =>
-      expect(err()).to.exist
-    )
-
-    it('adds the action', () =>
-      expect(err().action).to.equal('/users/123')
-    )
-
-    it('adds paperplane as the component', () =>
-      expect(err().component).to.equal('paperplane')
-    )
-
-    it('adds the httpMethod', () =>
-      expect(err().httpMethod).to.equal('GET')
-    )
-
-    it('adds the params', () =>
-      expect(err().params).to.eql({ id: 123 })
-    )
-
-    it('adds the ua (user agent)', () =>
-      expect(err().ua).to.equal('Chrome')
-    )
-
-    it('adds the url', () =>
-      expect(err().url).to.equal('https://myapp.com/users/123?token=abc')
-    )
+    itAddsRequestDetailsTo(err)
   })
 
   describe('when rejection is a response and not an error', () => {
@@ -77,6 +83,53 @@ describe('paperplane-airbrake', () => {
     it('does not notify airbrake', () => {
       expect(airbrake.notify.calls.length).to.equal(0)
       expect(res().statusCode).to.equal(302)
+    })
+  })
+
+  describe('when notifying airbrake returns an error', () => {
+    let error
+    const notified = spy()
+
+    const airbrake = {
+      notify: (err, cb) => {
+        notified(err, cb)
+        cb(new Error('bad request'))
+      }
+    }
+
+    const cry = require('..')(airbrake)
+    const err = property()
+
+    before(() => {
+      error = console.error
+      console.error = spy()
+    })
+
+    beforeEach(() => {
+      console.error.reset()
+      notified.reset()
+
+      const bad = Object.assign(new Error('bad'), { req })
+      return cry(bad).catch(err)
+    })
+
+    after(() =>
+      console.error = error
+    )
+
+    it('re-notifies airbrake with the new error', () => {
+      expect(notified.calls.length).to.equal(2)
+      expect(notified.calls[1][0].message).to.equal('bad request')
+    })
+
+    const airbrakeErr =
+      partial(path(['calls', 1, 0]), [ notified ])
+
+    itAddsRequestDetailsTo(airbrakeErr)
+
+    it('logs any further errors', () => {
+      expect(console.error.calls.length).to.equal(1)
+      expect(console.error.calls[0][0].message).to.equal('bad request')
     })
   })
 })
